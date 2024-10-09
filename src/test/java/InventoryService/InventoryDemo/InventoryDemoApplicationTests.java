f00fa385d6068c4461e2c6f350ac5581cd9f48cd
Saptadwipa Choudhury <saptadweepa.30@gmail.com>
2024-10-09 21:15:51 +0200

    added files

---
+++ src/test/java/InventoryService/InventoryDemo/InventoryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package InventoryService.InventoryDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
